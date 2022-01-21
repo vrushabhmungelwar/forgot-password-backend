a23d244b135adb814a577b898564ed0d3bfe6a8b
Vrushabh Mungelwar <vrushabhmungelwar10@gmail.com>
2022-01-21 16:38:49 +0530

    initial commit

--- routes/forgotPassword.js
+++ routes/forgotPassword.js
@@ -50,7 +50,7 @@ router.get("/resetpassword/:id/:token", async (req, res) => {
     });
     if (!token) return res.status(400).send("Invalid link");
 
-    res.writeHead(301, { Location: "http://localhost:3000/resetpassword" });
+    res.writeHead(301, { Location: "https://xenodochial-curie-f10544.netlify.app/#/resetpassword" });
     res.end();
   } catch (error) {
     res.status(400).send("An error occured");
